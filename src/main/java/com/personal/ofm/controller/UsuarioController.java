6d01b652e1356c1cbaddc967857d254782670536
Brian Barrientos <brianbg232018@gmail.com>
2019-09-30 11:57:55 -0600

    CRUD de vistas completos

--- src/main/java/com/personal/ofm/controller/UsuarioController.java
+++ src/main/java/com/personal/ofm/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.personal.ofm.entity.Categorias;
 import com.personal.ofm.entity.Roles;
 import com.personal.ofm.entity.Usuarios;
 import com.personal.ofm.repository.IRoles;
@@ -37,20 +38,46 @@ public class UsuarioController {
 	
 	@GetMapping(value = "guardar")
 	public String nuevo(Model m) {
-		List<Roles> roles = (List<Roles>) iroles.findAll();
-		m.addAttribute("roles", roles);
 		return "Empleados/EmpleadosList";
 	}
 	
 	@PostMapping(value = "guardar")
-	public String guardar(@RequestParam String nombre, @RequestParam String dui, @RequestParam String telefono, @RequestParam Integer rol){
+	public String guardar(@RequestParam String nombre, @RequestParam String dui, @RequestParam String telefono, @RequestParam Integer roles){
 		@Valid Usuarios user = new Usuarios();
 		user.setNombre(nombre);
 		user.setDui(dui);
 		user.setTelefono(telefono);
-		Roles roles = iroles.findById(rol).get();
-		user.setIdRol(roles);
+		Roles rol = iroles.findById(roles).get();
+		user.setIdRol(rol);
 		iusuario.save(user);
+		return "redirect:/usuario/listar";
+	}
+	
+	@GetMapping(value = "modificar/{idUsuario}")
+	public String modificar(@PathVariable Long idUsuario, Model m) {
+		Usuarios user = iusuario.findById(idUsuario).get();
+		m.addAttribute("items", user);
+		List<Roles> roles = (List<Roles>) iroles.findAll();
+		m.addAttribute("roles", roles);
+		return "Empleados/EmpleadosEdit";
+	}
+	
+	@PostMapping(value = "modificar")
+	public String editar(@RequestParam Long idUsuario, @RequestParam String nombre, @RequestParam String dui, @RequestParam String telefono,@RequestParam Integer idRol) {
+		Usuarios user = new Usuarios();
+		user.setIdUsuario(idUsuario);
+		user.setNombre(nombre);
+		user.setDui(dui);
+		user.setTelefono(telefono);
+		Roles roles = iroles.findById(idRol).get();
+		user.setIdRol(roles);
+		return "redirect:/usuario/listar";
+	}
+	
+	@RequestMapping(value = "eliminar/{idUsuario}")
+	public String elimanar(@PathVariable Long idUsuario) {
+		Usuarios user = iusuario.findById(idUsuario).get();
+		iusuario.delete(user);
 		return "redirect:/usuario/listar";
 	}
 }
